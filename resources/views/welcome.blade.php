90345a1b65438b495eec114852a1d2f84240cd91
ecs87 <elsonsmith87@gmail.com>
2018-04-16 11:26:35 -0500

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+	<div class="logo">
+		<img src="http://motilecoin.io/wp-content/uploads/2018/02/coin-low-reolution.png">
+	</div>
+	Welcome to ECS87's MIE Web Wallet. To get started please create a new wallet, select a password, save your priv key, 
+	then log into (using the send coins link in the menu) the wallet you just created. Curious about a transaction? Feel free to use the TX lookup! 
+	Import wallet/private key functions coming soon.
+</div>
+
+@stop
